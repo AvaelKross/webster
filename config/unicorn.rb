404525fb0fcee6842aa2eb5573afce68aabcfe35
Alexey <avaelkross@gmail.com>
2014-07-07 21:25:24 +0600

    stupid trying

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ working_directory  app_path
 pid                "#{app_path}/tmp/pids/unicorn.pid"
 stderr_path        "/home/apps/webster/log/unicorn.err.log"
 stdout_path        "/home/apps/webster/log/unicorn.out.log"
-listen             "#{app_path}/shared/unicorn.sock", :backlog => 64
+listen             "#{app_path}/shared/unicorn.sock"
 
 before_fork do |server, worker|
   ActiveRecord::Base.connection.disconnect!
