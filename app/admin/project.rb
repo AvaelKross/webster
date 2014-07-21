dee440dfb0611a6a48bb764e4304477de22cc30a
Alexey <avaelkross@gmail.com>
2014-07-21 15:54:09 +0600

    permit sms_from

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Project do
 
-  permit_params :name, :site, :email, :phone, :email_on, :sms_on, :email_template, :sms_template, :last_sent, :sent_in_row, :email_sender_email, :email_sender, :email_subject
+  permit_params :name, :site, :email, :phone, :email_on, :sms_on, :email_template, :sms_template, :last_sent, :sent_in_row, :email_sender_email, :email_sender, :email_subject, :sms_from
   
 end
