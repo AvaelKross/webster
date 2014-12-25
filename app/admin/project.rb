76524a0836e622eb62076edfb00bb39f9f62c76e
Alexey <avaelkross@gmail.com>
2014-12-25 16:32:03 +0500

    params template-add

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Project do
 
-  permit_params :name, :site, :email, :phone, :email_on, :sms_on, :email_template, :sms_template, :last_sent, :sent_in_row, :email_sender_email, :email_sender, :email_subject, :sms_from, :bitrix_on, :bitrix_url, :bitrix_login, :bitrix_password, :bitrix_title_template, :bitrix_comment_template
+  permit_params :name, :site, :email, :phone, :email_on, :sms_on, :email_template, :sms_template, :last_sent, :sent_in_row, :email_sender_email, :email_sender, :email_subject, :sms_from, :bitrix_on, :bitrix_url, :bitrix_login, :bitrix_password, :bitrix_title_template, :bitrix_comment_template, :bitrix_params_template
   
 end
